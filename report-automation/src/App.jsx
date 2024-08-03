8ce04903ba496459ced67ead3b58492a5cd4ff64
yazan sawan <alisarter2@gmail.com>
2024-08-03 20:13:25 +0300

    dark theme

--- report-automation/src/App.jsx
+++ report-automation/src/App.jsx
@@ -1,9 +1,31 @@
-
+import { useEffect, useState } from "react";
 import ReportForm from "./Component/ReportForm";
 
-
 function App() {
-  return <ReportForm/>;
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme); // حفظ الثيم في اللوكـال ستورج
+  };
+
+  // استرجاع الثيم من اللوكـال ستورج عند تحميل الصفحة
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }, []);
+
+  return (
+    <>
+      <button onClick={toggleTheme}>
+        تغيير الثيم إلى {theme === "light" ? "داكن" : "فاتح"}
+      </button>
+      <ReportForm />
+    </>
+  );
 }
 
 export default App;
